0c90801d945379d468a92696dc3414290d027a9c
oni122 <fran.valdes@duocuc.cl>
2025-09-02 18:17:06 -0400

    prueba10

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
 import { defineConfig } from 'astro/config';
 import vercel from "@astrojs/vercel/serverless";
+import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  output: "server", 
   adapter: vercel(),
+  integrations: [tailwind()],
 });
