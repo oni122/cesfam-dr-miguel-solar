2fe6eecd8002b956942d744cacde8e03fc68e542
rockbell-E <jonathanilllanes@gmail.com>
2025-09-27 23:08:52 -0300

    Se agrego el calendario

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
 import { defineConfig } from "astro/config";
-import vercel from "@astrojs/vercel"; 
+import vercel from "@astrojs/vercel";
 import tailwind from "@astrojs/tailwind";
+
+
+import vue from "@astrojs/vue";
 
 
 export default defineConfig({
   adapter: vercel(),
-  integrations: [tailwind()],
+  integrations: [tailwind(), vue({
+    appEntrypoint: '/src/plugins/vue.js'
+  })],
 });
---
+++ src/components/Calendar.vue
@@ -0,0 +1,15 @@
+<template>
+  <VDatePicker v-model="date" :disabled-dates="disabledDates" />
+</template>
+
+<script setup>
+import { ref } from 'vue';
+const date = ref(new Date());
+const disabledDates = ref([
+  {
+    repeat: {
+      weekdays: [7,1],
+    },
+  },
+]);
+</script>
