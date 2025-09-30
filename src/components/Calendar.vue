a2ad22c03100b5d504973350ce2afce8a77292f4
rockbell-E <jonathanilllanes@gmail.com>
2025-09-29 21:34:50 -0300

    Se movio la estructura de la reserva al template de vue

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -1,16 +1,40 @@
 <template>
-  <VDatePicker v-model="date" mode="date" :disabled-dates="disabledDates" 
-  />
+  <div class="bg-[#200934] w-[90%] text-center py-4 rounded">
+    <div class="flex justify-around">
+        <button @click="showElement = !showElement" class="py-3 px-8 outline rounded outline-2 bg-[#2c0a71]">Por hora</button>
+        <button @click="showElement = !showElement" class="py-3 px-8 outline rounded outline-2 bg-[#2c0a71]">Por doctor</button>
+    </div>
+
+    <div :class="{ invisible: !showElement}">
+      <h2>Por hora</h2>
+      <VDatePicker v-model="date" mode="date" :disabled-dates="disabledDates" />
+
+    </div>
+    <div :class="{ invisible: showElement}">
+      <h2>Por Medico</h2>
+    </div>
+  </div>
 </template>
 
 <script setup>
+
+
 import { ref } from 'vue';
 const date = ref(new Date());
 const disabledDates = ref([
-  {
+{
     repeat: {
       weekdays: [1],
     },
   },
 ]);
+
+
+
 </script>
+
+<style>
+  .invisible {
+    display: none;
+  }
+</style>
